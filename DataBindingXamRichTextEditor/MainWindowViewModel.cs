4ffbdd53696e5c46a02327c4f0e6802c0db324dd
Brian Lagunas <brian.lagunas@live.com>
2015-05-14 17:02:05 -0600

    initial add

---
+++ DataBindingXamRichTextEditor/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataBindingXamRichTextEditor
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _htmlText;
+
+        public string HtmlText
+        {
+            get { return _htmlText; }
+            set
+            {
+                _htmlText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
